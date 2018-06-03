1f047fac1dbe3f8724ba44c8a7267ffae0a280d2
f3flight <f3flight@gmail.com>
2018-06-02 19:58:24 -0700

    Rework parsing

--- MakeyMakey/makeymakey.ino
+++ MakeyMakey/makeymakey.ino
@@ -14,23 +14,12 @@
 #include "makeymakey_pins.h"
 #include "utility.h"
 
-
-const char input_pins[] {
-  INPUT_D0, INPUT_D1, INPUT_D2, INPUT_D3, INPUT_D4, INPUT_D5, 
-  INPUT_CLICK, INPUT_SPACE, INPUT_AR_DN, INPUT_AR_UP, INPUT_AR_LT, INPUT_AR_RT,
-  INPUT_A0, INPUT_A1, INPUT_A2, INPUT_A3, INPUT_A4, INPUT_A5
-};
-
-const char led_pins[] {
-  LED_A, LED_B, LED_C, LED_D
-};
-
 bool input_state[NUM_INPUT_PINS];
 
 const char NUM_ACTIONS = 3;
 
 const int ACTION_NAME_LEN_MAX = 6;
-const int ACTION_KB_CODES_MAX = 7;
+const int ACTION_KB_CODES_MAX = 6;
 struct keyboard_payload {
   uint8_t len;
   uint8_t codes[ACTION_KB_CODES_MAX];
@@ -40,8 +29,8 @@ union payload {
   keyboard_payload keyboard;
 };
 
+typedef bool(*parse_function)(char*, const char, payload*);
 typedef bool(*action_function)(payload);
-typedef bool(*parse_function)(char*, char*, payload*);
 
 struct function_link {
   char action_name[ACTION_NAME_LEN_MAX];
@@ -49,12 +38,12 @@ struct function_link {
   parse_function parser;
 };
 
+bool parser_keyboard(char* input, const char delim, payload* p);
 bool action_keyboard(payload input);
-bool parser_keyboard(char* input, char* delims, payload* p);
 bool action_mouse(payload input);
 bool action_midi(payload input);
 
-const function_link function_map[] = {
+const function_link function_map[NUM_ACTIONS] = {
   {"kb", &action_keyboard, &parser_keyboard},
   {"mouse", &action_mouse},
   {"midi", &action_midi},
@@ -68,12 +57,11 @@ struct input_action {
 input_action pin_actions[NUM_PINS][4] = {};
 input_action run_action;
 
-const char delim_str[] = " ";
-const char inner_delim = ',';
-const char inner_delim_str[] = ",";
-const short read_buffer_len = 100;
-uint8_t read_buffer[read_buffer_len] = {0};
-uint8_t parse_buffer[read_buffer_len] = {0};
+const char DELIM = ' ';
+const char INNER_DELIM = ',';
+const short READ_BUFFER_LEN = 100;
+uint8_t read_buffer[READ_BUFFER_LEN] = {0};
+uint8_t parse_buffer[READ_BUFFER_LEN] = {0};
 short read_buffer_pos = 0;
 
 void setup() {
@@ -84,12 +72,10 @@ void setup() {
 }
 
 void loop() {
-    for (int i = 0; i < NUM_INPUT_PINS; i++)
-    {
-      bool state = (digitalRead(input_pins[i]) == LOW);
-      if (input_state[i] != state)
-      {
-        Serial.print("input "); Serial.print(input_pins[i]); Serial.print(" new state "); Serial.println(state);
+    for (int i = 0; i < NUM_INPUT_PINS; i++) {
+      bool state = (digitalRead(INPUT_PINS[i]) == LOW);
+      if (input_state[i] != state) {
+        Serial.print("input "); Serial.print(INPUT_PINS[i]); Serial.print(" new state "); Serial.println(state);
         input_state[i] = state;
         // int action = (state) ? 0x90 : 0x80;
         // midiEventPacket_t noteOn = {0x09, action | 0, 48 + i, 127};
@@ -97,136 +83,122 @@ void loop() {
         // MidiUSB.flush();
       }
     }
-    uint8_t input[read_buffer_len] = {0};
+    uint8_t input[READ_BUFFER_LEN] = {0};
     int out_len;
-    if (serial_readbytes_until(read_buffer, read_buffer_len, &read_buffer_pos, input, &out_len)) {
+    if (serial_readbytes_until(read_buffer, READ_BUFFER_LEN, &read_buffer_pos, input, &out_len)) {
       Serial.print("got a line ["); Serial.print((char*)input); Serial.println("]");
-      if (input[0] != '#') parse_input(input, delim_str);  // parsing input if it's not a comment
+      if (input[0] != '#') parse_input(input, DELIM);  // parsing input if it's not a comment
     } else {
       //Serial.println("no input this time..."); 
     }
     delay(50);
 }
 
-void parse_input(const char* input, const char* delims) {  // input must be a proper 0-terminated string
-  char* piece = strtok(input, delims);
-  int counter = 0;
+void parse_input(char* input, const char delim) {  // input must be a proper 0-terminated string
+  int buffer_pos = 0;
   bool get = false, set = false, test = false, run = false;
+  int counter = 0;
   int input_num = 0;
-  while (piece != NULL) {
-    counter++;
-    if (counter == 1) {
-      if (strcmp(piece, "get") == 0) {get = true;}
-      else if (strcmp(piece, "set") == 0) {set = true;}
-      else if (strcmp(piece, "test") == 0) {test = true;}
-      else if (strcmp(piece, "run") == 0) {run = true;}
-      else {Serial.println("don't know whatcha talkin' 'bout!"); break;}}  // unknown primary action, exiting
-    else if (run) {
-      // TODO: parse_action corrupts piece somehow, when it doesn't parse it till the end, need to investigate
-      // repro: run kb,47,48,49,50,51,52,53,54,55,56,57,58 --> will pring "running:" 2 times
-      Serial.print("running: "); Serial.println(piece);
-      action_function action;
-      payload p;
-      if (parse_action(piece, inner_delim, &action, &p)) {
-        Serial.println("action parsed, executing!");
+  action_function action;
+  payload p;
+  while (strtok_better(parse_buffer, READ_BUFFER_LEN, &buffer_pos, input, delim)) {
+    if (counter == 0) {
+      if (strcmp(parse_buffer, "get") == 0) {get = true;}
+      else if (strcmp(parse_buffer, "set") == 0) {set = true;}
+      else if (strcmp(parse_buffer, "test") == 0) {test = true;}
+      else if (strcmp(parse_buffer, "run") == 0) {run = true;}
+      else {
+        _pn("don't know whatcha talkin' 'bout!");
+        break;  // unknown primary action, exiting
+      }
+    } else if (run) {
+      _p("running: "); _pn(parse_buffer);
+      if (parse_action(parse_buffer, INNER_DELIM, &action, &p)) {
+        _pn("parsed action, executing");
         action(p);
       }
+    } else if (get || (set && counter == 1) || test) {
+      if (!str_to_int(parse_buffer, &input_num) || input_num < 0 || input_num > NUM_INPUT_PINS - 1) {
+        _pn("Are you trying to break me? This input pin number is bad.");
+        break;
+      }
+      //Serial.print("input pin = "); Serial.println(input_num);
+      if (get) {
+        _p("getting config of input "); _pn(input_num);
+      } else if (set) {
+        _p("clearing config for input "); _pn(input_num);
+      } else if (test) {
+        _p("simulating trigger of input "); _pn(input_num);
+      }
+    } else if (set) {
+      _p("setting config for input "); _p(input_num); _p(": "); _pn(parse_buffer);
     }
-    else if (counter == 2) {
-      if (str_to_int(piece, &input_num) && input_num >= 0 && input_num < NUM_INPUT_PINS-1) {
-        //Serial.print("input pin = "); Serial.println(input_num);
-        if (get) {
-          Serial.print("getting config of input "); Serial.println(input_num);
-          break;}  // dumped config, ignoring rest of input
-        else if (set) {
-          Serial.print("clearing config for input "); Serial.println(input_num);}
-        else if (test) {
-          Serial.print("simulating trigger of input "); Serial.println(input_num);
-          break;}}  // done simulation, ignoring rest of input
-      else {
-        Serial.println("Are you trying to break me? This input pin number is bad.");
-        break;}}  // exiting due to incorrect input
-    else if (counter >= 3) {
-      if (set) {
-        Serial.print("setting for input "); Serial.print(input_num); Serial.print(": "); Serial.println(piece);}}  
-    piece = strtok(NULL, delims);
+    counter++;
   }
-  if (get && counter == 1) {
-    Serial.println("getting config of all inputs...");
-  } 
 }
 
 bool parse_action(char* input, const char delim, action_function* action, payload* p) {
-  memset(parse_buffer, 0, read_buffer_len);
-  memcpy(parse_buffer, input, strlen(input));
-  int input_len = strlen(parse_buffer);  // length of current (single) action piece
-  char* name_len = strchr(parse_buffer, delim);
+  _pn("parsing action");
+  bool found = false;
   parse_function parser = nullptr;
-  bool action_found = false;
-  if (name_len != NULL) {
-    *name_len = 0;  //zero out first found delimiter to convert action name into 0-terminated string
-  }
-  for (int i=0; i < NUM_ACTIONS; i++) {
-    if (strcmp(parse_buffer, function_map[i].action_name) == 0) {
-      *action = function_map[i].action;
-      parser = function_map[i].parser;
-      action_found = true;
-      Serial.print("found action - "); Serial.println(function_map[i].action_name); 
-      break;
-    }
-  }
-  if (action_found) {
-    if (parser != nullptr) {  // action has a parser, should have payload
-      Serial.println("action has a parser");
-      if (input_len > strlen(parse_buffer) + 1) {  // there is some payload for this action
-        Serial.println("there is some payload for this action");
-        if (parser(parse_buffer+strlen(parse_buffer)+1, inner_delim_str, p)) {
-          Serial.println("was able to parse!");
-          return true;
-        }
+  int buffer_pos = 0;
+  int len = strlen(input);
+  if (strtok_better(parse_buffer, READ_BUFFER_LEN, &buffer_pos, input, delim)) {
+    for (int i=0; i < NUM_ACTIONS; i++) {
+      if (strcmp(parse_buffer, function_map[i].action_name) == 0) {
+        *action = function_map[i].action;
+        parser = function_map[i].parser;
+        _p("found action - "); _pn(function_map[i].action_name);
+        if (parser != nullptr) {  // action has a parser, need to parse payload
+          if (buffer_pos < len) {  // there is some payload for this action
+            _pn("there is some payload for this action");
+            if (parser(input+buffer_pos+1, delim, p)) {
+              _pn("was able to parse!");
+              return true;
+            }
+          }
+        } else return true;  // action without parser = no payload parsing needed
+        break;
       }
-    } else return true;
+    }
   }
   return false;
 }
 
-bool action_keyboard(payload p) {
-  for (int i=0; i < p.keyboard.len; i++) {
-    // TODO: press all won't ahndle more than 6 keys, looks like a general limitation, need to decide what to do
-    // decrease codes array or add extra handling for modifier keys
-    Keyboard.press(p.keyboard.codes[i]);
-    Keyboard.release(p.keyboard.codes[i]);
-  }
-  //Keyboard.releaseAll();
-}
-
-bool parser_keyboard(char* input, char* delims, payload* p) {
-  Serial.println("parsing kb action section");
-  char* piece = strtok(input, delims);
+bool parser_keyboard(char* input, const char delim, payload* p) {
+  _pn("parsing kb action section");
   int counter = 0;
-  while (piece != NULL) {
-    Serial.println("found a piece");
+  int buffer_pos = 0;
+  while (strtok_better(parse_buffer, READ_BUFFER_LEN, &buffer_pos, input, delim)) {
+    _p("found a piece: "); _pn(parse_buffer);
     int code;
-    if (str_to_int(piece, &code) && code >= 0) {
-      Serial.println("decoded int");
+    if (str_to_int(parse_buffer, &code) && code >= 0) {
+      _pn("decoded int");
       if (counter < ACTION_KB_CODES_MAX) {
         p->keyboard.codes[counter] = code;
         counter++;
       } else {  // reached limit of codes array size, cannot store any more actions
-        Serial.println("Reached limit of codes per keyboard action, add new action instead.");
+        _pn("Reached limit of codes per keyboard action:"); _p(ACTION_KB_CODES_MAX); _pn("; Add new action instead.");
         p->keyboard.len = ACTION_KB_CODES_MAX;
         return true;
       }
     } else {
-      Serial.println("wrong code for keyboard input, ignoring action payload");
+      _pn("wrong code for keyboard input, ignoring action payload");
       break;
     }
-    piece = strtok(NULL, delims);
   }
   p->keyboard.len = counter;
-  Serial.print("p->keyboard.len = "); Serial.println(p->keyboard.len);
-  Serial.print("counter > 0"); Serial.println(counter > 0);
+  _p("p->keyboard.len = "); _pn(p->keyboard.len);
+  _p("counter > 0"); _pn(counter > 0);
   return counter > 0;
+}
+
+bool action_keyboard(payload p) {
+  for (int i=0; i < p.keyboard.len; i++) {
+    Keyboard.press(p.keyboard.codes[i]);
+    //Keyboard.release(p.keyboard.codes[i]);
+  }
+  Keyboard.releaseAll();
 }
 
 bool action_mouse(payload p) {
--- MakeyMakey/utility.h
+++ MakeyMakey/utility.h
@@ -1,1 +1,107 @@
-// Non-blocking line or byte reader utilizing external buffer and buffer position. Version 1.1.2018.05.31.0// Defaults for optional parameters are to work in line reading mode. Cast output to (char*) and there you go.// params://   buffer: byte array pointer to store temporary data until a delimiter is found//   buffer_len: length of buffer array//   buffer_pos: int pointer to track and update the current position in the buffer array//   out: byte array pointer to copy data to when delimiter is detected//   out_len (opt): int pointer, returns length of data in output, excluding delimiter (which is also copied to out);             //   overflow_keep (opt): if false (default), will drop such data and all further data until delimiter is hit (i.e. ignoring too long data),//                        if true, will return data chunk (without delimiter) when buffer size is reached, no data is lost in this case;//                        you can use buffer_pos to detect overflow when overflow_keep = true://                        if (buffer_pos < 0) {} // overflow happened//   delim (opt): delimiter to look for, defaults to newline symbol '\n'//   delim_to_zero (opt): if true (default), will replace delimiter with 0 byte '\0'//                        if false, delimeter is copied to out as-is//   drop_special_byte (opt): if true (default), skip a specific byte value, if found//   special_byte (opt): which byte value to skip, works when drop_special_byte = true. Defaults to carriage return symbol '\r'.//   max_wait_millis (opt): after how many milliseconds to stop reading serial and return false;//                          this allows using the function in a loop, with low cost of each call,//                          accumulating data over time and eventually returning it when delimiter is found.//bool serial_readbytes_until(uint8_t buffer[], int buffer_len, int* buffer_pos, uint8_t out[], int* out_len = nullptr, bool overflow_keep = false,                            uint8_t delim = '\n', bool delim_to_zero = true, bool drop_special_byte = true, uint8_t special_byte = '\r',                            int max_wait_millis = 10) {    int32_t time = millis();  bool read = false;  if (*buffer_pos < 0) *buffer_pos = 0;  // resetting overflow indication  while (Serial.available() > 0) {    if (millis() - time > max_wait_millis && read) break;  // locking protection on high data inflow, but allow at least 1 byte to be read    if (*buffer_pos >= buffer_len) {  // too much data, buffer exhausted, need to decide what to do      if (overflow_keep) {  // returning accumulated data chunk without delimiter        memcpy(out, buffer, buffer_len);        *out_len = buffer_len;        *buffer_pos = -1;  // indicating overflow        return true;      } else {        uint8_t x = Serial.read();        if (x == delim) {*buffer_pos = 0;}  // dropping all accumulated data and all subsequent data until delimiter is found        continue;      }    }    uint8_t x = Serial.read();    if (drop_special_byte && x == special_byte) continue;    read = true;    buffer[*buffer_pos] = x;    if (x == delim) {  // found delimiter      if (delim_to_zero) buffer[*buffer_pos] = '\0';  // replacing delimiter with 0 byte      if (*buffer_pos > 0) {  // have data, returning result        memcpy(out, buffer, 1 + (*buffer_pos));        if (out_len != nullptr) *out_len = *buffer_pos;        *buffer_pos = 0;        return true;      } else {*buffer_pos = 0; continue;}  // there was no data before delimiter, skipping and resetting buffer position    }    (*buffer_pos)++;  }  return false;}#include <limits.h>#include <errno.h>// based on https://wiki.sei.cmu.edu/confluence/display/c/ERR34-C.+Detect+errors+when+converting+a+string+to+a+numberbool str_to_int(const char* buff, int* out_int) {  char *end;  errno = 0;  const long sl = strtol(buff, &end, 10);  if (end == buff) {    //fprintf(stderr, "%s: not a decimal number\n", buff);  } else if ('\0' != *end) {    //fprintf(stderr, "%s: extra characters at end of input: %s\n", buff, end);  } else if ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno) {    //fprintf(stderr, "%s out of range of type long\n", buff);  } else if (sl > INT_MAX) {    //fprintf(stderr, "%ld greater than INT_MAX\n", sl);  } else if (sl < INT_MIN) {    //fprintf(stderr, "%ld less than INT_MIN\n", sl);  } else {    *out_int = (int)sl;    return true;  }  return false;}
+// Non-blocking line or byte reader utilizing external buffer and buffer position. Version 1.1.2018.05.31.0
+// Defaults for optional parameters are to work in line reading mode. Cast output to (char*) and there you go.
+// params:
+//   buffer: byte array pointer to store temporary data until a delimiter is found
+//   buffer_len: length of buffer array
+//   buffer_pos: int pointer to track and update the current position in the buffer array
+//   out: byte array pointer to copy data to when delimiter is detected
+//   out_len (opt): int pointer, returns length of data in output, excluding delimiter (which is also copied to out);
+//   overflow_keep (opt): if false (default), will drop such data and all further data until delimiter is hit (i.e. ignoring too long data),
+//                        if true, will return data chunk (without delimiter) when buffer size is reached, no data is lost in this case;
+//                        you can use buffer_pos to detect overflow when overflow_keep = true:
+//                        if (buffer_pos < 0) {} // overflow happened
+//   delim (opt): delimiter to look for, defaults to newline symbol '\n'
+//   delim_to_zero (opt): if true (default), will replace delimiter with 0 byte '\0'
+//                        if false, delimeter is copied to out as-is
+//   drop_special_byte (opt): if true (default), skip a specific byte value, if found
+//   special_byte (opt): which byte value to skip, works when drop_special_byte = true. Defaults to carriage return symbol '\r'.
+//   max_wait_millis (opt): after how many milliseconds to stop reading serial and return false;
+//                          this allows using the function in a loop, with low cost of each call,
+//                          accumulating data over time and eventually returning it when delimiter is found.
+//
+bool serial_readbytes_until(uint8_t buffer[], int buffer_len, int* buffer_pos, uint8_t out[], int* out_len = nullptr, bool overflow_keep = false,
+                            uint8_t delim = '\n', bool delim_to_zero = true, bool drop_special_byte = true, uint8_t special_byte = '\r',
+                            int max_wait_millis = 10) {
+  int32_t time = millis();
+  bool read = false;
+  if (*buffer_pos < 0) *buffer_pos = 0;  // resetting overflow indication
+  while (Serial.available() > 0) {
+    if (millis() - time > max_wait_millis && read) break;  // locking protection on high data inflow, but allow at least 1 byte to be read
+    if (*buffer_pos >= buffer_len) {  // too much data, buffer exhausted, need to decide what to do
+      if (overflow_keep) {  // returning accumulated data chunk without delimiter
+        memcpy(out, buffer, buffer_len);
+        *out_len = buffer_len;
+        *buffer_pos = -1;  // indicating overflow
+        return true;
+      } else {
+        uint8_t x = Serial.read();
+        if (x == delim) {*buffer_pos = 0;}  // dropping all accumulated data and all subsequent data until delimiter is found
+        continue;
+      }
+    }
+    uint8_t x = Serial.read();
+    if (drop_special_byte && x == special_byte) continue;
+    read = true;
+    buffer[*buffer_pos] = x;
+    if (x == delim) {  // found delimiter
+      if (delim_to_zero) buffer[*buffer_pos] = '\0';  // replacing delimiter with 0 byte
+      if (*buffer_pos > 0) {  // have data, returning result
+        memcpy(out, buffer, 1 + (*buffer_pos));
+        if (out_len != nullptr) *out_len = *buffer_pos;
+        *buffer_pos = 0;
+        return true;
+      } else {*buffer_pos = 0; continue;}  // there was no data before delimiter, skipping and resetting buffer position
+    }
+    (*buffer_pos)++;
+  }
+  return false;
+}
+
+#include <limits.h>
+#include <errno.h>
+// based on https://wiki.sei.cmu.edu/confluence/display/c/ERR34-C.+Detect+errors+when+converting+a+string+to+a+number
+bool str_to_int(const char* buff, int* out_int) {
+  char *end;
+  errno = 0;
+  const long sl = strtol(buff, &end, 10);
+  if (end == buff) {
+    //fprintf(stderr, "%s: not a decimal number\n", buff);
+  } else if ('\0' != *end) {
+    //fprintf(stderr, "%s: extra characters at end of input: %s\n", buff, end);
+  } else if ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno) {
+    //fprintf(stderr, "%s out of range of type long\n", buff);
+  } else if (sl > INT_MAX) {
+    //fprintf(stderr, "%ld greater than INT_MAX\n", sl);
+  } else if (sl < INT_MIN) {
+    //fprintf(stderr, "%ld less than INT_MIN\n", sl);
+  } else {
+    *out_int = (int)sl;
+    return true;
+  }
+  return false;
+}
+
+bool strtok_better(char buffer[], int buffer_len, int* buffer_pos, char* input, const char delim) {
+  int last_pos = *buffer_pos;
+  while (*(input + *buffer_pos) != 0) {
+    (*buffer_pos)++;
+    if (*(input + *buffer_pos ) == delim && *(input + *buffer_pos - 1) != delim) break;  // stopping on delimiter if it's not right after another delimiter
+    if (*buffer_pos - last_pos >= buffer_len) {return false;}  // string won't fit into buffer, returning false;
+  }
+  if (last_pos != *buffer_pos) {
+    memcpy(buffer, input + last_pos, *buffer_pos - last_pos);
+    buffer[*buffer_pos - last_pos] = 0;
+    if (*(input + *buffer_pos) != 0) (*buffer_pos)++;
+    return true;
+  }
+  return false;
+}
+
+void _p(const uint8_t b[]) {Serial.print((char*)b);}
+void _pn(const uint8_t b[]) {Serial.println((char*)b);}
+void _p(const char* s) {Serial.print(s);}
+void _pn(const char* s) {Serial.println(s);}
+void _p(const char c) {Serial.print(c);}
+void _pn(const char c) {Serial.println(c);}
+void _p(const int i) {Serial.print(i);}
+void _pn(const int i) {Serial.println(i);}
